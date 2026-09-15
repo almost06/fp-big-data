7b9dc33dc9aab8c8b8387d655a235bf8a5783d12
Alex <haosen909@gmail.com>
2026-09-15 15:02:16 +0200

    Final

---
+++ src/main/scala/dataset/Dataset.scala
@@ -0,0 +1,149 @@
+package dataset
+
+import dataset.util.Commit.{Commit, CommitData, File, Parent, Stats, User}
+import java.text.SimpleDateFormat
+import java.util.SimpleTimeZone
+
+
+import java.text.SimpleDateFormat
+import java.util.SimpleTimeZone
+import scala.math.Ordering.Implicits._
+
+/**
+ * Use your knowledge of functional programming to complete the following functions.
+ * You are recommended to use library functions when possible.
+ *
+ * The data is provided as a list of `Commit`s. This case class can be found in util/Commit.scala.
+ * When asked for dates, use the `commit.commit.committer.date` field.
+ *
+ * This part is worth 40 points.
+ */
+object Dataset {
+
+
+  /** Q23 (4p)
+   * For the commits that are accompanied with stats data, compute the average of their additions.
+   * You can assume a positive amount of usable commits is present in the data.
+   *
+   * @param input the list of commits to process.
+   * @return the average amount of additions in the commits that have stats data.
+   */
+  def avgAdditions(input: List[Commit]): Int = {
+    val mapped = input.flatMap(x => x.stats).map(x => x.additions)
+    mapped.sum / mapped.length
+  }
+
+  /** Q24 (4p)
+   * Find the hour of day (in 24h notation, UTC time) during which the most javascript (.js) files are changed in commits.
+   * The hour 00:00-00:59 is hour 0, 14:00-14:59 is hour 14, etc.
+   * NB!filename of a file is always defined.
+   * Hint: for the time, use `SimpleDateFormat` and `SimpleTimeZone`.
+   *
+   * @param input list of commits to process.
+   * @return the hour and the amount of files changed during this hour.
+   */
+  def jsTime(input: List[Commit]): (Int, Int) = {
+    def classifier(c : Commit): Int = {
+      val formatter = new SimpleDateFormat("HH")
+      formatter.setTimeZone(new SimpleTimeZone(0, "UTC"))
+
+      formatter.format(c.commit.committer.date).toInt
+    }
+    if(input.nonEmpty){
+      val map = input.groupBy(classifier)
+      val filtered = map.mapValues(x => x.map(t => t.files.map(s => {
+        if (s.filename.getOrElse("").endsWith(".js")) 1
+        else 0
+      }).sum
+      ))
+      filtered.mapValues(x => x.sum).maxBy(t => t._2) // finds the biggest count and returns as a tuple the hour and the amount of files
+    }
+    else (0, 0)
+  }
+
+
+  /** Q25 (5p)
+   * For a given repository, output the name and amount of commits for the person
+   * with the most commits to this repository.
+   * For the name, use `commit.commit.author.name`.
+   *
+   * @param input the list of commits to process.
+   * @param repo  the repository name to consider.
+   * @return the name and amount of commits for the top committer.
+   */
+  def topCommitter(input: List[Commit], repo: String): (String, Int) = {
+    def classifier(c : Commit): String = {
+      c.commit.author.name
+    }
+    val map = input.filter(t=> {
+      val x = t.url.split("/")
+      x(4) + "/" + x(5) == repo
+    }).groupBy(classifier).mapValues(x => x.length)
+    map.maxBy(t => t._2)
+  }
+
+  /** Q26 (9p)
+   * For each repository, output the name and the amount of commits that were made to this repository in 2019 only.
+   * Leave out all repositories that had no activity this year.
+   *
+   * @param input the list of commits to process.
+   * @return a map that maps the repo name to the amount of commits.
+   *
+   *         Example output:
+   *         Map("KosDP1987/students" -> 1, "giahh263/HQWord" -> 2)
+   */
+
+  def commitsPerRepo(input: List[Commit]): Map[String, Int] = {
+    def classifier(c : Commit): String = {
+      val list = c.url.split("/") // example url from our github: https://github.com/almost06/fp-big-data/commit/a4e82614639068fdebc3a7d7c972965dc4ef63da
+      list(4) + "/" + list(5)
+    }
+    val map = input.groupBy(classifier).mapValues(t => t.count(x => x.commit.committer.date.getYear == 119))
+    map.filter{case(key, value) => value > 0}
+  }
+
+  /** Q27 (9p)
+   * Derive the 5 file types that appear most frequent in the commit logs.
+   * NB!filename of a file is always defined.
+   * @param input the list of commits to process.
+   * @return 5 tuples containing the file extension and frequency of the most frequently appeared file types, ordered descendingly.
+   */
+  def topFileFormats(input: List[Commit]): List[(String, Int)] = {
+    val list = input.flatMap(x => {
+      x.files.map(t => t.filename.getOrElse("").replaceAll("^.*\\.", ""))
+    }).groupBy(identity).mapValues(x => x.length).toList
+
+    list.sortBy(t => t._2).reverse.take(5)
+  }
+
+
+  /** Q28 (9p)
+   *
+   * A day has different parts:
+   * morning 5 am to 12 pm (noon)
+   * afternoon 12 pm to 5 pm.
+   * evening 5 pm to 9 pm.
+   * night 9 pm to 4 am.
+   *
+   * Which part of the day was the most productive in terms of commits ?
+   * Return a tuple with the part of the day and the number of commits
+   *
+   * Hint: for the time, use `SimpleDateFormat` and `SimpleTimeZone`.
+   */
+  def mostProductivePart(input: List[Commit]): (String, Int) = {
+    def classifier(c: Commit): String = {
+      val formatter = new SimpleDateFormat("HH")
+      formatter.setTimeZone(new SimpleTimeZone(0, "UTC"))
+
+      val hour = formatter.format(c.commit.committer.date).toInt
+      hour match {
+        case x if x >= 5 && x < 12 => "morning"
+        case x if x >= 12 && x < 17 => "afternoon"
+        case x if x >= 17 && x < 21 => "evening"
+        case x if x >= 21 || x < 5 => "night"
+      }
+    }
+    input.groupBy(classifier).mapValues(x => x.length).maxBy(t => t._2)
+
+  }
+}
---
+++ src/main/scala/intro/Practice.scala
@@ -0,0 +1,97 @@
+package intro
+
+import scala.annotation.tailrec
+
+/**
+  * This part has some exercises for you to practice with the recursive lists and functions.
+  * For the exercises in this part you are _not_ allowed to use library functions,
+  * you should implement everything yourself.
+  * Use recursion to process lists, iteration is not allowed.
+  *
+  * This part is worth 16 points.
+  */
+object Practice {
+
+    /** Q10 (2p)
+      * Implement the function that returns the first `n` elements from the list.
+      * Note that `n` is an upper bound, the list might not have `n` elements.
+      *
+      * @param xs list to take items from.
+      * @param n amount of items to take.
+      * @return the first n items of xs.
+      */
+    def firstN(xs: List[Int], n: Int): List[Int] = xs match {
+      case Nil => Nil
+      case _ if n == 0 => Nil
+      case x :: tail if n > 0 => x :: firstN(tail, n-1)
+    }
+
+
+    /** Q11 (4p)
+      * Implement the function that returns the maximum value in the list.
+      * If the list is empty, return `Int.MinValue`
+      *
+      * @param xs list to process.
+      * @return the maximum value in the list.
+      */
+    def maxValue(xs: List[Int]): Int = {
+      @tailrec
+      def recursion(xs: List[Int], m: Int): Int = xs match {
+          case Nil => m
+          case x :: tail if(x > m) => recursion(tail, x)
+          case x :: tail if(x <= m) => recursion(tail, m)
+      }
+      if(xs == Nil) Int.MinValue
+      else recursion(xs, Int.MinValue)
+    }
+
+    /** Q12 (3p)
+     * given two Ints, generate the List[Int] with both numbers inclusive
+     *
+     * Examples:
+     * intList(2,7) // List(2,3,4,5,6,7)
+     * intList(3,0) // List()
+     */
+    def intList(a: Int, b: Int) : List[Int] = {
+      def recursion(xs: List[Int], end: Int): List[Int] = xs match {
+        case Nil => Nil
+        case x :: Nil if x < end => x :: recursion(List(x+1), end)
+        case x :: Nil if x == end => x :: Nil
+      }
+      if(a > b) List()
+
+      else recursion(List(a), b)
+    }
+
+    /**
+     * Q13 (7p)
+     * This question is a variant on a filter function. Given a List[A] and a
+     * function f: A => Boolean, `myFilter` should retain all elements from
+     * the list which satisfy 'f' and throw out all other elements. From the
+     * resulting list, each element with an odd numbered index should also
+     * be thrown out (we start counting the index at 0, of course).
+     *
+     * Take a look at the examples to see more directly what it needs to do
+     * if you find this description vague.
+     *
+     * You are required to solve this using pattern matching on lists.
+     * HINT: define a 'helper' method within myFilter which uses case matching.
+     *
+     * Examples:
+     * 	val nrs = List.range(0,11) // List(0,1,2,3,...,10)
+     * 	myFilter(nrs, (i: Int) => i % 2 == 0) // List(0,4,8)
+     *
+     * so although 2, 6 and 10 satisfy the function, they are thrown out.
+     */
+    // a helper method which you've written yourself
+    def myFilter[A](xs: List[A], f: A => Boolean) : List[A] = {
+      def firstRecursion(list: List[A], f: A => Boolean, counter: Int) : List[A] = list match {
+        case Nil => Nil
+        case x :: tail if(f(x) && counter%2 == 0) => x :: firstRecursion(tail, f, counter+1)
+        case x :: tail if f(x) && counter%2 != 0 => firstRecursion(tail, f, counter+1)
+        case x :: tail if !f(x) => firstRecursion(tail, f, counter)
+      }
+
+      firstRecursion(xs, f, 0)
+    }
+}
